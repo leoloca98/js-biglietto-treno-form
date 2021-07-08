155ad3e3481e4cef88d412e613bf5d7eb73a1607
leoloca98 <leonardolocatelli98@gmail.com>
2021-07-08 19:14:27 +0200

    Generate Button Add

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@
 *Quali campi inserire nel form:
 // Una casella di testo per il form del passeggero
 // Una casella numerica per i km
-Una tendina per selezionare la fascia d'età (Maggiorenne, Minorenne, Over65)
+// Una tendina per selezionare la fascia d'età (Maggiorenne, Minorenne, Over65)
 NON E' NECESSARIA ALCUNA VALIDAZIONE
 
 *Come calcolare  il prezzo:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali,
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali)
 
 *Cosa stampare sul biglietto:
-Nome passeggero
+// Nome passeggero
 Codice treno (inseriamo una stringa a caso, per ora)
 Numero carrozza (randomizziamo una carrozza da 1 a 12)
 Prezzo (stampato con due decimali)
@@ -29,37 +29,52 @@ Aggiungiamo una funzione che ci permetta di resettare i campi del form ai valori
 */
 
 
-// ! Chiediamo il numero di chilometri che l'utente ha intenzione di percorrere
+// ?FORM
+var nameField = document.getElementById("full-name");
+var kmsField = document.getElementById("kms");
+var ageField = document.getElementById("age");
+var generate = document.getElementById("generate");
+var reset = document.getElementById("reset");
 
-// var x = prompt("Quanti chilometri vuoi fare?");
-var userKm = parseFloat(x);
-console.log(userKm);
+
+// ?ELEMENTS
+var userNameElement = document.getElementById("user-name");
+var discountElement = document.getElementById("discount");
+var carElement = document.getElementById("car");
+var codeCPElement = document.getElementById("code-cp");
+var ticketPriceElement = document.getElementById("ticket-price");
+
+// ?TICKET-PRICE
+var discountedPrice = 0;
+var finalPrice = 0;
+var tariffa = "";
+
+// ?CLICK
+generate.addEventListener('click',
+    function () {
+        var nameValue = nameField.value;
+        var ageValue = ageField.value;
+        var kmsValue = kmsField.value;
+        var initialPrice = (0.21 * kmsValue);
+
+        if (ageValue == "Minorenne") {
+            discountedPrice = initialPrice - (initialPrice * 0.2);
+            tariffa = "Tariffa Minorenne";
+
+        } else if (ageValue == "Over-65") {
+            discountedPrice = initialPrice - (initialPrice * 0.4);
+            tariffa = "Tariffa Over-65";
+        } else {
+            discountedPrice = initialPrice;
+            tariffa = "Tariffa Ordinaria";
+        }
+
+        discountedPrice = discountedPrice.toFixed(2);
+        userNameElement.innerHTML = nameValue;
+        discountElement.innerText = tariffa;
+        carElement.innerHTML = Math.floor(Math.random() * 12) + 1;
+        codeCPElement.innerText = "95876";
+        ticketPriceElement.innerHTML = discountedPrice;
+    });
 
 
-// ! Chiediamo l'età del passeggero
-// var y = prompt("Quanti hanni hai?");
-var userAge = parseFloat(y);
-console.log(userAge)
-
-// ! Calcoliamo il prezzo del biglietto (0.21€ al km)
-var initialPrice = (0.21 * userKm);
-console.log(initialPrice);
-
-// ! Controlliamo l'età dell'utente
-var discountedPrice;
-var finalPrice;
-// Se Minorenne
-if (userAge < 18) {
-    discountedPrice = initialPrice - (initialPrice * 0.2);
-    // Se over 65
-} else if (userAge >= 65) {
-    discountedPrice = initialPrice - (initialPrice * 0.4);
-} else if (userAge >= 18 && userAge < 65) {
-    discountedPrice = initialPrice;
-}
-
-discountedPrice = discountedPrice.toFixed(2);
-
-finalPrice = document.getElementById("price");
-finalPrice.innerText = discountedPrice;
-console.log(discountedPrice);
